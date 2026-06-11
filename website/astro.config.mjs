5ef057a0a10ca25b04005347d8c8a74c0cbfb0bf
Owais Jamil <desertthunder.dev@gmail.com>
2026-06-10 23:15:00 -0500

    chore: format website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,6 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://marker.stormlightlabs.org',
-});
+export default defineConfig({ site: 'https://marker.stormlightlabs.org' });
